5339d107928ff6746054a38d19ee3234cc6108c8
D-n-1 <datbtph36315@fpt.edu.vn>
2023-11-13 22:52:03 +0700

    Play

---
+++ app/src/main/java/com/example/oder_food/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.oder_food;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // đạt đẹp trai
+    }
+}
